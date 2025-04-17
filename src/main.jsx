c576e2e67562f8c2669fcb69ea7b2dacd940fd34
nihar dukale <nihardukale@gmail.com>
2025-04-17 22:39:58 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AppContextProvider from './Context/AppContext.jsx'
+import AppContextProvider from './Context/AppContext'
 import {DeviceContextProvider} from './Context/deviceContext.jsx'
 
 import {BrowserRouter} from 'react-router-dom'
