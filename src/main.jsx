0e7e41fa9b6396a76d37006f70f42f857a8c60ed
nihar dukale <nihardukale@gmail.com>
2025-04-17 23:08:35 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AppContextProvider from './Context/AppContext'
 import DeviceContextProvider from './Context/Device.jsx';
-
-
 import {BrowserRouter} from 'react-router-dom'
 import CartProvider from './Context/CartContext.jsx'
 import UserContextProvider from './Context/UserContext.jsx'
