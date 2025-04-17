3ef94a1e1dd47ccfa4599172ef27393d5487f268
nihar dukale <nihardukale@gmail.com>
2025-04-17 22:43:35 +0530

    Temp rename to fix casing issue

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AppContextProvider from './Context/AppContext'
-import {DeviceContextProvider} from './Context/deviceContext.jsx'
+import DeviceContextProvider from './Context/deviceContext.jsx';
+
 
 import {BrowserRouter} from 'react-router-dom'
 import CartProvider from './Context/CartContext.jsx'
